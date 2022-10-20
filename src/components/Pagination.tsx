d3d8fc9797dd0b22d42a7be0ed4f5ab1c4173cd3
Yuriy <batm1x1@gmail.com>
2022-10-20 22:22:03 +0300

    adaptive 320p

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,20 +1,76 @@
-import React from "react";
-import "../assets/scss/Pagination.scss";
+import React from 'react';
+import '../assets/scss/Pagination.scss';
 
 type PropsType = {
-  setActivePage: (number, string) => void;
-  activePage: number;
+   setActivePage: (number, string) => void;
+   activePage: number;
 };
 const Pagination: React.FC<PropsType> = ({ setActivePage, activePage }) => {
-  return (
-    <div className="pagination">
-      <div className="pagination__pages">0{activePage}/06</div>
-      <div className="pagination__buttons">
-        <span onClick={() => setActivePage(activePage, "previous")}></span>
-        <span onClick={() => setActivePage(activePage, "next")}></span>
+   const nextPage = activePage === 6 ? 1 : activePage + 1;
+   const prevPage = activePage === 1 ? 6 : activePage - 1;
+   return (
+      <div className="pagination">
+         <div className="pagination__pages">0{activePage}/06</div>
+         <div className="pagination__buttons">
+            <span
+               onClick={() => setActivePage(activePage, prevPage)}
+            ></span>
+            <span
+               onClick={() => setActivePage(activePage, nextPage)}
+            ></span>
+            <div className="pagination__mobile">
+               <div
+                  className={
+                     activePage === 1
+                        ? 'pagination__mobile-circle activePage'
+                        : 'pagination__mobile-circle'
+                  }
+                  onClick={() => setActivePage(activePage, 1)}
+               ></div>
+               <div
+                  className={
+                     activePage === 2
+                        ? 'pagination__mobile-circle activePage'
+                        : 'pagination__mobile-circle'
+                  }
+                  onClick={() => setActivePage(activePage, 2)}
+               ></div>
+               <div
+                  className={
+                     activePage === 3
+                        ? 'pagination__mobile-circle activePage'
+                        : 'pagination__mobile-circle'
+                  }
+                  onClick={() => setActivePage(activePage, 3)}
+               ></div>
+               <div
+                  className={
+                     activePage === 4
+                        ? 'pagination__mobile-circle activePage'
+                        : 'pagination__mobile-circle'
+                  }
+                  onClick={() => setActivePage(activePage, 4)}
+               ></div>
+               <div
+                  className={
+                     activePage === 5
+                        ? 'pagination__mobile-circle activePage'
+                        : 'pagination__mobile-circle'
+                  }
+                  onClick={() => setActivePage(activePage, 5)}
+               ></div>
+               <div
+                  className={
+                     activePage === 6
+                        ? 'pagination__mobile-circle activePage'
+                        : 'pagination__mobile-circle'
+                  }
+                  onClick={() => setActivePage(activePage, 6)}
+               ></div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Pagination;
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,42 +1,46 @@
-import React, { useState } from "react";
-import "swiper/css/bundle";
-import "../assets/scss/Slider.scss";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, A11y } from "swiper";
+import React, { useState } from 'react';
+import 'swiper/css/bundle';
+import '../assets/scss/Slider.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, A11y } from 'swiper';
+import { SlidersType } from '../components/App';
 
-export const Slider = (currentSlides) => {
-  const [prevEl, setPrevEl] = useState<HTMLElement | null>(null);
-  const [nextEl, setNextEl] = useState<HTMLElement | null>(null);
+export const Slider: React.FC<{ currentSlides: SlidersType }> = ({
+   currentSlides,
+}) => {
+   const [prevEl, setPrevEl] = useState<HTMLElement | null>(null);
+   const [nextEl, setNextEl] = useState<HTMLElement | null>(null);
 
-  const slides = currentSlides.currentSlides.map((item, index) => (
-    <SwiperSlide key={index}>
-      <div className="slide">
-        <span>{item[0]}</span>
-        <p>{item[1]}</p>
+   const slides = currentSlides.slides.map((item, index) => (
+      <SwiperSlide key={index}>
+         <div className="slide">
+            <span>{item[0]}</span>
+            <p>{item[1]}</p>
+         </div>
+      </SwiperSlide>
+   ));
+   return (
+      <div className="slider">
+         <div className="slider-title">{currentSlides.title}</div>
+         <Swiper
+            modules={[Navigation, A11y]}
+            slidesPerView={window.innerWidth > 320 ? 3 : 2}
+            navigation={{
+               prevEl,
+               nextEl,
+               disabledClass: 'disabled_swiper_button',
+            }}
+         >
+            {slides}
+         </Swiper>
+         <span
+            className="slider-button prev-button"
+            ref={(node) => setPrevEl(node)}
+         ></span>
+         <span
+            className="slider-button next-button"
+            ref={(node) => setNextEl(node)}
+         ></span>
       </div>
-    </SwiperSlide>
-  ));
-  return (
-    <div className="slider">
-      <Swiper
-        modules={[Navigation, A11y]}
-        slidesPerView={3}
-        navigation={{
-          prevEl,
-          nextEl,
-          disabledClass: "disabled_swiper_button",
-        }}
-      >
-        {slides}
-      </Swiper>
-      <span
-        className="slider-button prev-button"
-        ref={(node) => setPrevEl(node)}
-      ></span>
-      <span
-        className="slider-button next-button"
-        ref={(node) => setNextEl(node)}
-      ></span>
-    </div>
-  );
+   );
 };
